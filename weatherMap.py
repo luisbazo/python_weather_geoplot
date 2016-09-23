3b20d7e76e38c6bee82b610646da0f9494c3d34c
luis.bazo <luis.bazo@es.ibm.com>
2016-09-23 14:33:52 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	gmplotopenweather.pyc
    #	index.html

--- weatherMap.py
+++ weatherMap.py
@@ -1,4 +1,5 @@
 import gmplotopenweather as gmplot
+from pymongo import MongoClient
 import pandas as pd
 import pyowm
 import urllib2,json
@@ -18,27 +19,33 @@ apigooglemaps=opts.apigooglemaps
 #Center defaut map in Europe and zoom 6 times
 gmap = gmplot.GoogleMapPlotter(48.51, 2.2, 5)
 owm = pyowm.OWM(api)
-
-df = pd.read_csv('cities.csv')
+df = pd.read_csv('country-list.csv')
 df.head()
 
 for index, row in df.iterrows():
-  loc_city = str(df.loc[index,"city"]) + "," + str(df.loc[index,"country"])
+  loc_city = str(df.loc[index,"capital"]) + "," + str(df.loc[index,"country"])
+  #loc_city = str(df.loc[index,"city"])
   iterate = True
   counter = 0
   while (iterate and counter < 2):
     try:
         print "Retrieving information of city " + loc_city
         #location = geolocator.geocode(df.loc[index,"city"])
-        lat,lng = gmap.getCoordinates(df.loc[index,"city"],apigooglemaps)
+        lat,lng = gmap.getCoordinates(loc_city,apigooglemaps)
+        #print "Data ", data
+        #if(saveData):
+        #	client = MongoClient()
+        #    client.test.weather.insert_one(jsonloads)
+        #    cursor = client.test.weather.find({"city":city})
         #observation = owm.weather_at_coords(location.latitude,location.longitude)
         observation = owm.weather_at_coords(lat,lng)
         w = observation.get_weather()
         jsonweather = w.to_JSON()
         jsonloads = json.loads(jsonweather)
 
+        loc_city_html = loc_city.replace("'","")
         temp = jsonloads["temperature"]["temp"] - 273.15
-        text = '<h1><b>Weather forecast for ' + loc_city + '</b></h1><p></p><p><b>Today</b> ' + str(jsonloads['detailed_status']) + ' ' + str(temp) + ' C'
+        text = '<h1><b>Weather forecast for ' + loc_city_html + '</b></h1><p></p><p><b>Today</b> ' + str(jsonloads['detailed_status']) + ' ' + str(temp) + ' C'
 
         fc = owm.daily_forecast_at_coords(lat, lng)
         fcweather = fc.get_forecast()
